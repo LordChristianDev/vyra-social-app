c07f9e8a279727652ec833682a73a4d82d7eb1be
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-06 23:58:53 +0800

    first commit

---
+++ client/vite.config.ts
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
---
+++ mongo.js
@@ -0,0 +1,11 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGO_CONNECTION;
+
+export const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
+});
