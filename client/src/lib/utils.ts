7c1b5ed4463f752e710ed05f08517b5a52daff31
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-18 23:43:40 +0800

    Updated VITE Configuruations and Stylings

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -3,4 +3,11 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+};
+
+export const scrollToSection = (id: string) => {
+  const element = document.getElementById(id)
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" })
+  }
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,21 +3,45 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
-})
+    define: {
+      'process.env': loadEnv(mode, '../'),
+    },
+  }
+});
+
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//     tailwindcss()
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   define: {
+//     'process.env': loadEnv(mode, '../'),
+//   },
+// })
