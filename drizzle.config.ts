d5859f6914b747eaff15c7e6936d4449d0d8ae6a
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-23 21:06:50 +0800

    Integrated Drizzle Config

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+	dialect: 'postgresql',
+	out: './drizzle',
+	schema: './src/db/schema.ts',
+	dbCredentials: {
+		host: process.env.PGHOST!,
+		user: process.env.PGUSER!,
+		password: process.env.PGPASSWORD!,
+		database: process.env.PGDATABASE!,
+		ssl: true,
+	},
+});
