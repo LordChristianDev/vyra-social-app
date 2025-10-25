c0836546691e81d624b22c24e5460d1b222211d3
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-25 20:56:16 +0800

    Integrated Message Route

--- src/modules/messages/routes/message-route.ts
+++ src/modules/messages/routes/message-route.ts
@@ -1,5 +1,18 @@
+import { Context } from 'koa';
 import Router from '@koa/router';
-import { MUTATIONS, QUERIES } from '@/modules/messages/server/message-server';
+
+import {
+	SelectConversation,
+	InsertParticipant,
+	SelectParticipant,
+	InsertMessage,
+	SelectMessage,
+} from "@/db/schema";
+
+import {
+	QUERIES,
+	MUTATIONS,
+} from '@/modules/messages/server/message-server';
 
 const router = new Router();
 
@@ -12,9 +25,194 @@ router.get('/', (ctx) => {
  * Create
  */
 
+router.post('/create-conversation', async (ctx) => {
+	const [data, error] = await MUTATIONS.createConversation();
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to create conversation",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.post('/create-participant/:profile_id', async (ctx) => {
+	const profile_id: InsertParticipant["profile_id"] = Number(ctx.params.profile_id);
+	const request_body = ctx.request.body as { conversation_id: InsertParticipant["conversation_id"] };
+	const { conversation_id } = request_body;
+
+	const [data, error] = await MUTATIONS.createParticipant(
+		profile_id,
+		conversation_id,
+	);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to create participant",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.post('/create-message', async (ctx: Context) => {
+	const request_body = ctx.request.body as InsertMessage;
+	const { conversation_id, sender_id, receiver_id, content } = request_body;
+
+	const [data, error] = await MUTATIONS.createMessage(
+		conversation_id,
+		sender_id,
+		receiver_id,
+		content,
+	);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to create message",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
 /**
  * Retrieve
  */
+
+router.get("fetch-converations-profile-id/:profile_id", async (ctx) => {
+	const profile_id: SelectParticipant["profile_id"] = Number(ctx.params.profile_id);
+
+	const [data, error] = await QUERIES.fetchConversationsByProfileId(profile_id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to fetch conversations",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.get("fetch-converation/:id", async (ctx) => {
+	const id: SelectConversation["id"] = Number(ctx.params.id);
+
+	const [data, error] = await QUERIES.fetchConversationById(id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to fetch conversation",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.get("fetch-participants-conversation-id/:conversation_id", async (ctx) => {
+	const conversation_id: SelectParticipant["conversation_id"] = Number(ctx.params.conversation_id);
+
+	const [data, error] = await QUERIES.fetchParticipantsByConversationId(conversation_id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to fetch participants",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.get("fetch-participants-profile-id/:profile_id", async (ctx) => {
+	const profile_id: SelectParticipant["profile_id"] = Number(ctx.params.profile_id);
+
+	const [data, error] = await QUERIES.fetchParticipantsByProfileId(profile_id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to fetch participants",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.get("fetch-messages/:conversation_id", async (ctx) => {
+	const conversation_id: SelectMessage["conversation_id"] = Number(ctx.params.conversation_id);
+
+	const [data, error] = await QUERIES.fetchMessagesByConversationId(conversation_id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: "Failed to fetch messages",
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
 
 /**
  * Update
@@ -23,3 +221,71 @@ router.get('/', (ctx) => {
 /**
  * Delete
  */
+
+router.delete('/delete-conversation/:id', async (ctx) => {
+	const id: SelectConversation["id"] = Number(ctx.params.id);
+
+	const [data, error] = await MUTATIONS.deleteConversationsWithId(id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: 'Failed to delete conversation',
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.delete('/delete-participant/:id', async (ctx) => {
+	const id: SelectParticipant["id"] = Number(ctx.params.id);
+
+	const [data, error] = await MUTATIONS.deleteParticipantsWithId(id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: 'Failed to delete participant',
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+router.delete('/delete-message/:id', async (ctx) => {
+	const id: SelectMessage["id"] = Number(ctx.params.id);
+
+	const [data, error] = await MUTATIONS.deleteMessagesWithId(id);
+
+	if (error) {
+		ctx.status = 500;
+		ctx.body = {
+			status: false,
+			error: 'Failed to delete message',
+			message: error.message,
+		};
+		return;
+	}
+
+	ctx.status = 200;
+	ctx.body = {
+		status: true,
+		data,
+	};
+});
+
+export default router;
