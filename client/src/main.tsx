645ba8bd7d92ff8dd1313efd0bb8c1457ed6edbb
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-29 17:19:07 +0800

    Integrated Clerk Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { StackProvider } from '@stackframe/react';
+import { ClerkProvider } from '@clerk/clerk-react'
 import { QueryClient, QueryClientProvider, } from '@tanstack/react-query';
-import { stackClientApp } from "./lib/stack-auth.ts";
 
 import './index.css';
 import App from './App.tsx';
+
+export const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:4000"
+  : "";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <StackProvider app={stackClientApp}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
           <App />
-        </StackProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ClerkProvider>
   </StrictMode>,
 );
