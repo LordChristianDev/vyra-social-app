029961ce540419bbe83ccadae827ae3e35e4088e
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-29 17:21:41 +0800

    Removed StackTheme Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import { StackTheme } from "@stackframe/react";
 
 import { AuthProvider } from "@/context/use-auth";
 import { ThemeProvider } from "@/context/use-theme";
@@ -27,28 +26,26 @@ export default function App() {
         <ProfileProvider>
           <TooltipProvider>
             <Toaster />
-            <StackTheme>
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="*" element={
-                  <ProtectedRoute>
-                    <AuthenticatedLayout>
-                      <Routes>
-                        <Route path="/home" element={<HomePage />} />
-                        <Route path="/explore" element={<ExplorePage />} />
-                        <Route path="/messages" element={<MessagesPage />} />
-                        <Route path="/notifications" element={<NotificationsPage />} />
-                        <Route path="/profile" element={<ProfilePage />} />
-                        <Route path="/view-profile/:id" element={<ViewProfilePage />} />
-                        <Route path="/settings" element={<SettingsPage />} />
-                        <Route path="*" element={<NotFound />} />
-                      </Routes>
-                    </AuthenticatedLayout>
-                  </ProtectedRoute>
-                } />
-              </Routes>
-            </StackTheme>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="*" element={
+                <ProtectedRoute>
+                  <AuthenticatedLayout>
+                    <Routes>
+                      <Route path="/home" element={<HomePage />} />
+                      <Route path="/explore" element={<ExplorePage />} />
+                      <Route path="/messages" element={<MessagesPage />} />
+                      <Route path="/notifications" element={<NotificationsPage />} />
+                      <Route path="/profile" element={<ProfilePage />} />
+                      <Route path="/view-profile/:id" element={<ViewProfilePage />} />
+                      <Route path="/settings" element={<SettingsPage />} />
+                      <Route path="*" element={<NotFound />} />
+                    </Routes>
+                  </AuthenticatedLayout>
+                </ProtectedRoute>
+              } />
+            </Routes>
           </TooltipProvider>
         </ProfileProvider>
       </AuthProvider>
