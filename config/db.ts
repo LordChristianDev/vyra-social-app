06571659b076033fd0b3816ccb97578e2f70d180
LordChristianDev <lordchristian.dev@gmail.com>
2025-10-30 19:02:19 +0800

    Updated Drizzle Config

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,13 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const {
+	PGHOST,
+	PGDATABASE,
+	PGUSER,
+	PGPASSWORD
+} = process.env;
+
 const sql = neon(
 	`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
 );
