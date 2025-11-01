bdad677c83aff7082ee7e65a338d540cc0867348
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-01 23:48:54 +0800

    Updated All Message Queries

--- src/modules/messages/server/message-server.ts
+++ src/modules/messages/server/message-server.ts
@@ -1,4 +1,4 @@
-import { eq, desc } from "drizzle-orm";
+import { eq, desc, and, not, notInArray, inArray } from "drizzle-orm";
 
 import { db } from "@config/db";
 import { queryDB } from "@/utils/try-catch";
@@ -12,10 +12,11 @@ import {
 	InsertMessage,
 	SelectMessage,
 	messagesTable as MESSAGES_TABLE,
+	profilesTable as PROFILES_TABLE,
 } from "@/db/schema";
 
 export const QUERIES = {
-	fetchConversationsByProfileId: async function (
+	fetchAllConversationsByProfileId: async function (
 		profile_id: SelectParticipant["profile_id"]
 	) {
 		return queryDB(async () => {
@@ -35,12 +36,82 @@ export const QUERIES = {
 			return result;
 		});
 	},
+	fetchSomeConversationsByProfileId: async function (
+		profile_id: SelectParticipant["profile_id"],
+		page: number,
+		pageSize: number,
+	) {
+		return queryDB(async () => {
+			const result = await db
+				.select({
+					id: CONVERSATIONS_TABLE.id,
+					created_at: CONVERSATIONS_TABLE.created_at,
+					updated_at: CONVERSATIONS_TABLE.updated_at,
+				})
+				.from(CONVERSATIONS_TABLE)
+				.innerJoin(
+					PARTICIPANTS_TABLE,
+					eq(PARTICIPANTS_TABLE.conversation_id, CONVERSATIONS_TABLE.id)
+				)
+				.where(eq(PARTICIPANTS_TABLE.profile_id, profile_id))
+				.limit(pageSize)
+				.offset((page - 1) * pageSize);
+
+			return result;
+		});
+	},
 	fetchConversationById: async function (id: SelectConversation["id"]) {
 		return queryDB(async () => {
 			const result = await db
 				.select()
 				.from(CONVERSATIONS_TABLE)
 				.where(eq(CONVERSATIONS_TABLE.id, id));
+
+			return result;
+		});
+	},
+	fetchMostRecentConversation: async function () {
+		return queryDB(async () => {
+			const result = await db
+				.select()
+				.from(CONVERSATIONS_TABLE)
+				.orderBy(desc(CONVERSATIONS_TABLE.created_at))
+				.limit(1);
+
+			return result;
+		});
+	},
+	fetchConversationBetweenProfiles: async function (
+		profile_id: SelectParticipant["profile_id"],
+		user_id: SelectParticipant["profile_id"]
+	) {
+		return queryDB(async () => {
+			const result = await db
+				.select({
+					id: CONVERSATIONS_TABLE.id,
+					created_at: CONVERSATIONS_TABLE.created_at,
+					updated_at: CONVERSATIONS_TABLE.updated_at,
+				})
+				.from(CONVERSATIONS_TABLE)
+				.where(
+					and(
+						inArray(
+							CONVERSATIONS_TABLE.id,
+							db
+								.select({ conversation_id: PARTICIPANTS_TABLE.conversation_id })
+								.from(PARTICIPANTS_TABLE)
+								.where(eq(PARTICIPANTS_TABLE.profile_id, profile_id))
+						),
+						inArray(
+							CONVERSATIONS_TABLE.id,
+							db
+								.select({ conversation_id: PARTICIPANTS_TABLE.conversation_id })
+								.from(PARTICIPANTS_TABLE)
+								.where(eq(PARTICIPANTS_TABLE.profile_id, user_id))
+						)
+					)
+				)
+				.limit(1);
 
 			return result;
 		});
@@ -78,6 +149,37 @@ export const QUERIES = {
 				.from(MESSAGES_TABLE)
 				.where(eq(MESSAGES_TABLE.conversation_id, conversation_id))
 				.orderBy(desc(MESSAGES_TABLE.sent_at));
+
+			return result;
+		});
+	},
+	fetchProfilesWithoutConversationWithUserId: async function (
+		user_id: SelectParticipant["profile_id"]
+	) {
+		return queryDB(async () => {
+			const result = await db
+				.select()
+				.from(PROFILES_TABLE)
+				.where(
+					and(
+						not(eq(PROFILES_TABLE.user_id, user_id)),
+						notInArray(
+							PROFILES_TABLE.user_id,
+							db
+								.selectDistinct({ profile_id: PARTICIPANTS_TABLE.profile_id })
+								.from(PARTICIPANTS_TABLE)
+								.where(
+									inArray(
+										PARTICIPANTS_TABLE.conversation_id,
+										db
+											.select({ conversation_id: PARTICIPANTS_TABLE.conversation_id })
+											.from(PARTICIPANTS_TABLE)
+											.where(eq(PARTICIPANTS_TABLE.profile_id, user_id))
+									)
+								)
+						)
+					)
+				);
 
 			return result;
 		});
